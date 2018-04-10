113def5fab1fdde1d3fd74f5677a90bf6eb97353
Fridolin Pokorny <fridolin@redhat.com>
2018-04-10 08:28:04 +0200

    Adjust startup script to use indexes and schema

--- images/janusgraph/startup.sh
+++ images/janusgraph/startup.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
 # Make sure we mark this container as ready once we have schema and indexes in place.
+set -ex
+
 touch /tmp/janusgraph-openshift-notready
 
 BIN=/opt/janusgraph-0.2.0-hadoop2/bin
 FILES=/opt/janusgraph-0.2.0-hadoop2/files
 $BIN/janusgraph.sh start -v
+#$BIN/janusgraph.sh start
 
-# Prepare schema and indexes.
 echo "Preparing schema..." 2>&1
 $BIN/gremlin.sh -e $FILES/schema.groovy
 
-echo "Preparing indexes..." 2>&1
-$BIN/gremlin.sh -e $FILES/indexes.groovy
+if [ -n "$GREMLIN_USE_INDEXES" ]; then
+	echo "Preparing indexes..." 2>&1
+
+	for file in $FILES/indexes/*.groovy;
+		do  sh gremlin.sh -e $file
+	done
+
+	sh gremlin.sh -e $FILES/submit-reindex-jobs.groovy
+
+	echo "Waiting for indexes to be propagated to cluster..." 2>&1; sleep 120
+	sh gremlin.sh -e $FILES/wait-for-indexes.yaml
+fi
+
 
 echo "Container is ready now" 2>&1
 rm /tmp/janusgraph-openshift-notready
