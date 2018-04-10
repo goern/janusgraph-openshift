25b0d6a387170f76bc86cb668112766cd34c6022
Fridolin Pokorny <fridolin@redhat.com>
2018-04-10 08:28:45 +0200

    Remove old indexes file

--- images/janusgraph/hack/files/indexes.groovy
+++
@@ -1,7 +0,0 @@
-// JanusGraph connects to localhost by default, that is what we want.
-
-:remote connect tinkerpop.server conf/remote.yaml session
-:remote console
-
-mgmt = graph.openManagement()
-
